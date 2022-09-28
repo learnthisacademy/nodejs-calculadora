d9b05bb5f95526710e49319de157bf49b9e24563
3rooks <Brookslynx@gmail.com>
2022-09-27 22:42:39 -0300

    🐛 Fix exponential operator

--- src/constants/operations.js
+++ src/constants/operations.js
@@ -3,7 +3,7 @@ export const operations = {
     '-': (firstOperating, secondOperating) => firstOperating - secondOperating,
     '*': (firstOperating, secondOperating) => firstOperating * secondOperating,
     '/': (firstOperating, secondOperating) => firstOperating / secondOperating,
-    '^': (firstOperating, secondOperating) => firstOperating ^ secondOperating,
+    '^': (firstOperating, secondOperating) => firstOperating ** secondOperating,
     log: (operating) => Math.log10(operating),
     sqrt: (operating) => Math.sqrt(operating),
 };
